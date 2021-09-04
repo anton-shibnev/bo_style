5855324668f50a2fd09da244d0c2f97f68e2212a
Anton Shibnev <shibnev.anton@gmail.com>
2021-09-04 20:33:38 +0300

    fix

--- fillCommonMixins.js
+++ fillCommonMixins.js
@@ -1,14 +1,13 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
+import path from 'path';
 
 const IGNORED_PROPERTIES = ['--*'];
-const MISSING_PROPERTIES = ['grid-gap', 'grid-column-gap', 'grid-row-gap'];
 
 class FillCommonMixin {
   constructor({ fileName }) {
-    // this.url = 'https://www.w3.org/Style/CSS/all-properties.en.json';
     this.url =
-      'https://gist.githubusercontent.com/davidhund/3bd6757d6a36a283b0a2933666bd1976/raw/4ccc47ed835c1be6bf0bfe00a32f427874da917c/all-css-properties.json';
+      'https://raw.githubusercontent.com/shibnev/all-css-properties/master/allCssProperties.json';
 
     this.fileName = fileName;
 
@@ -28,20 +27,13 @@ class FillCommonMixin {
       const result = [];
 
       list.forEach((item, index) => {
-        if (!IGNORED_PROPERTIES.includes(item.property)) {
+        if (!IGNORED_PROPERTIES.includes(item)) {
           if (index === 0) {
-            result.push(this.mixinTemplate(item.property));
-          } else if (
-            index !== 0 &&
-            item.property !== list[index - 1].property
-          ) {
-            result.push(this.mixinTemplate(item.property));
+            result.push(this.mixinTemplate(item));
+          } else if (index !== 0 && item.property !== list[index - 1]) {
+            result.push(this.mixinTemplate(item));
           }
         }
-      });
-
-      MISSING_PROPERTIES.forEach((item) => {
-        result.push(this.mixinTemplate(item));
       });
 
       return result;
